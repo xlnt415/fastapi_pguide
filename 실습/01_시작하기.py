bfe9db04e89b5cb70c640c527d9f0d307feeb282
Woo <97427612+xlnt415@users.noreply.github.com>
2025-09-02 18:47:43 +0900

    실습

--- 실습/01_시작하기.py
+++ 실습/01_시작하기.py
@@ -3,6 +3,9 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/")
+@app.get("/", summary="간단한 API", tags=["Simple"])#, description="매우 간단한 API임")
 async def root():
+    '''
+    이것은 간단한 API임
+    '''
     return {"message": "Hello World"}
